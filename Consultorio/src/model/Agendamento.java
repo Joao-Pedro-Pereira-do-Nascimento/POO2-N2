34e4f7ec53948d6f0562b402c9c28bd94cf0f04b
WallysonCR <contato.wallyson@hotmail.com>
2024-09-22 13:59:11 -0300

    Atualização em Agendamento {Atributos e construtor}

--- Consultorio/src/model/Agendamento.java
+++ Consultorio/src/model/Agendamento.java
@@ -1,9 +1,34 @@
 package model;
 
+import java.util.Date;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author wallyson
  */
-public class Agendamento {
-    
+public class Agendamento{
+    private static int contador = 0; // Elemento static
+    private int idAgendamento;
+    private Date dataHora;
+    private Paciente paciente;
+    private Medico medico;
+    private StatusAgendamento status;
+
+    public Agendamento(Date dataHora, Paciente paciente, Medico medico) {
+        this.idAgendamento = ++contador; // Incrementa o contador para gerar o ID
+        this.dataHora = dataHora;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.status = StatusAgendamento.PENDENTE; // Status padrão
+        JOptionPane.showMessageDialog(null, "Agendamento gerado. A consulta está a confirmar!");
+    }
+
+    static int getContador() {
+        return contador;
+    }
+
+    static void setContador(int contador) {
+        Agendamento.contador = contador;
+    }
 }
