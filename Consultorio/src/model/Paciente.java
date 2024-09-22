726012930ffee5552ab29526a097238dff8584e1
WallysonCR <contato.wallyson@hotmail.com>
2024-09-22 13:24:23 -0300

    Paciente {Atributos, construtor, getters e setters criados} + exibirDados implementado.

--- Consultorio/src/model/Paciente.java
+++ Consultorio/src/model/Paciente.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 /**
  *
  * @author wallyson
  */
-public class Paciente {
+public class Paciente extends Pessoa {
+    private String dataNascimento;
+    private String telefone;
+    private String email;
     
+    public Paciente(String nome, String sexo, String dataNascimento) {
+        super(nome, sexo);
+        this.dataNascimento = dataNascimento;
+        this.telefone = "Não informado";
+        this.email = "Não Informado";
+    }
+    
+    public Paciente(String nome, String sexo, String dataNascimento, String telefone) {
+        super(nome, sexo);
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = "Não informado";
+    }
+
+    public Paciente(String nome, String sexo, String dataNascimento, String telefone, String email) {
+        super(nome, sexo);
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void exibirDados() {
+        System.out.println("Paciente: " + getNome() + ", Sexo: " + sexo +
+                ", Data de Nascimento: " + dataNascimento + ", Telefone: " + telefone + ", Email: " + email);
+    }
 }
