48bce9b27944c501147e93e0bdc793d7453e08d9
WallysonCR <contato.wallyson@hotmail.com>
2024-09-22 15:22:47 -0300

    DatabaseConnection criado

---
+++ Consultorio/src/database/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author wallyson
+ */
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/Consultorio";
+    private static final String USER = "devt";
+    private static final String PASSWORD = "1234@Local";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }  
+}
