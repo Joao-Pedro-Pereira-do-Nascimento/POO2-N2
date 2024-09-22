a29b599c616c7d59c67baebcde6e5957ef107202
WallysonCR <contato.wallyson@hotmail.com>
2024-09-22 11:53:52 -0300

    Criação do construtor e dos getters e setters de Pessoa

--- Consultorio/src/model/Pessoa.java
+++ Consultorio/src/model/Pessoa.java
@@ -8,4 +8,33 @@ public class Pessoa {
     protected int id;
     protected String nome;
     protected String sexo;
+    
+    public Pessoa(String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 }
